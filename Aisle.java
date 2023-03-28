15b4e5e1c27679255207c6587dbaaf20f0bfaaf5
Shivanie <shivanie_kemraj@hotmail.com>
2023-03-28 17:40:04 -0400

    copy frame

---
+++ Aisle.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Aisle {
+    ArrayList<Item> items;
+
+    public void addItem(Item i){}
+    public Item getItem(Integer i){return new Item();}
+    public void removeItem(){}
+}
