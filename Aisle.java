026a8ca90f3fe5fa2c97377e5db457eba235fb2a
Shivanie <shivanie_kemraj@hotmail.com>
2023-04-05 15:52:55 -0400

    copy frame

--- Aisle.java
+++ Aisle.java
@@ -2,9 +2,19 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Aisle {
-    ArrayList<Item> items;
-
-    public void addItem(Item i){}
-    public Item getItem(Integer i){return new Item();}
-    public void removeItem(){}
+    private String aisleId;
+    private ArrayList<Item> items = new ArrayList<Item>();
+    public Aisle() {}
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+    public String getAisleId() {
+        return aisleId;
+    }
+    public void setAisleId(String s){
+        this.aisleId = s;
+    }
+    public void addItem(Item i){
+        items.add(i);
+    }
 }
