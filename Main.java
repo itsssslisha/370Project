fba51495edb16793514b1ebdc429744dd2d3482d
1rajveer1 <rajveer9905@gmail.com>
2023-04-05 21:31:57 -0400

    implemented strategy patern

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Warehouse warehouse = new Warehouse();
-        warehouse.addSection(new Section());
-        warehouse.addSection(new Section());
-        warehouse.addAisle('B', new Aisle());
-        warehouse.addItem("B1", new Item());
-    }
-}
--- getItemId.java
+++
@@ -1,8 +0,0 @@
-public class getItemId {
-    private int itemID = 0;
-    public int nextItemID() {
-
-        itemID++;
-        return itemID;
-    }
-}
