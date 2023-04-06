89f95c9dcc0acb34627b8672eff60eb9c59656e2
Shivanie <shivanie_kemraj@hotmail.com>
2023-04-06 00:25:27 -0400

    Updated working files

--- Item.java
+++ Item.java
@@ -1,28 +1,25 @@
-import java.net.Inet4Address;
+import Categories.Category;
+import Conditions.Condition;
 
 public class Item {
-    private String name;
-    private Integer cost;
-    private Integer quantity;
-    private String sellBy;
-    private Boolean Used;
-    private Integer itemID;
 
-    public Item(){
+    private Integer itemID;
+    private String name;
+    private Category category;
+    private Condition condition;
+    private Double Price;
+    public Item(String name, Double Price, Category category, Condition condition){
+        this.Price = Price;
         itemID = new getItemId().nextItemID();
+        System.out.println(name);
+        System.out.println("$" + Price);
+        category.showCategory();
+        condition.itemCondition();
+        System.out.println("");
     }
 
     public Integer getItemID() {
         return itemID;
     }
-//    public Item(String n, Integer c, Integer q, String sb, Boolean u, Integer id) {
-//        name = n;
-//        cost = c;
-//        quantity = q;
-//        sellBy = sb;
-//        Used = u;
-//    }
-
-
 
 }
